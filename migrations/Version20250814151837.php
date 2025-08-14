44c608c546a489a2dd61f4f20b340db52777be82
Sam Dev <samdev419@gmail.com>
2025-08-14 18:37:09 +0300

    - added contact form - added send email controller

---
+++ migrations/Version20250814151837.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250814151837 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE contact_form_type CHANGE text message VARCHAR(1255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE contact_form_type CHANGE message text VARCHAR(1255) NOT NULL');
+    }
+}
