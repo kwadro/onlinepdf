b8adb194a35eba1b9b9cf48ed9a723c567593fbe
Sam Dev <samdev419@gmail.com>
2025-09-28 21:17:57 +0300

    - correct api request

--- src/Controller/Api/ProfilerController.php
+++ src/Controller/Api/ProfilerController.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller\Api;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
@@ -16,22 +18,22 @@ class ProfilerController extends AbstractController
         private TranslatorInterface $translator
     ) {
     }
+
     #[Route('/api/me', name: 'api_profiler', methods: ['GET'])]
-//    #[IsGranted('IS_AUTHENTICATED')]
-    public function me(): Response
-    {
-        $user = $this->getUser();
+    #[IsGranted('IS_AUTHENTICATED')]
+    public function me(
+        #[CurrentUser] ?User $user
+    ): Response {
         if (!$user) {
             $message = [
-                'status'=>'error',
-                'text'=>$this->translator->trans('register.not_found_message', [], 'messages')
+                'status' => 'error',
+                'text' => $this->translator->trans('register.not_found_message', [], 'messages')
             ];
-            return $this->json(['message' => $message],404);
-
+            return $this->json(['message' => $message], 404);
         }
 
         return $this->json([
-            'id'    => $user->getId(),
+            'id' => $user->getId(),
             'email' => $user->getEmail(),
             'first_name' => $user->getFirstName(),
             'last_name' => $user->getLastName(),
